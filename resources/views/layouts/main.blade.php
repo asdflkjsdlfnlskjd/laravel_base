ce14e8554737690bc7de99cd08b6d16e4e6dce01
Kirill_Bitcoin <kirillperehodcev684@gmail.com>
2025-09-22 23:41:20 +0300

    Урок 16. Bootstrap в laravel

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,18 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">   
     <title>Document</title>
 </head>
 <body>
-    <div>
-        <div><nav>
-            <ul>
-                <li><a href="{{route('main.index')}}">Main</a></li>
-                <li><a href="{{route('post.index')}}">Posts</a></li>
-                <li><a href="{{route('about.index')}}">About</a></li>
-                <li><a href="{{route('contact.index')}}">Contacts</a></li>
-            </ul>
-        </nav></div>
+    <div class="container" >
+        <div class="row" >
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Laravel</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="{{route('main.index')}}">Main</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('post.index')}}">Posts</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('about.index')}}">About</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="{{route('contact.index')}}">Contacts</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>  
+    </div>
     @yield('content')
     </div>
 </body>
