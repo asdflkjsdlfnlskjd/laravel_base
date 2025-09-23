49b57455b1cc9a57800d8883ff457f4a4c094c8c
Kirill_Bitcoin <kirillperehodcev684@gmail.com>
2025-09-23 12:05:01 +0300

    Урок 17. CRUD через интерефейс. Имена роутов, контроллеров по конвенции

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,55 +12,60 @@ class   PostController extends Controller
        
         $posts = Post::all();
         
-        return view('posts', compact('posts'));
+        return view('post.index', compact('posts'));
 
         }
 
         public function create()
         {
-            $postsArr = [
-                [
-                    'title' => 'title of post from phpshtorm',
-                    'content' => 'some interesting content',
-                    'image' => 'imageblabla.jpg',
-                    'likes' => 52,
-                    'is_published' => 1,
-                ],
-                [
-                    'title' => 'another title of post from phpshtorm',
-                    'content' => 'another some interesting content',
-                    'image' => 'another imageblabla.jpg',
-                    'likes' => 25,
-                    'is_published' => 1,
-                ],
-            ];
+            return view('post.create');
+        }
 
-            foreach ($postsArr as $item) {
+        public function store() {
 
-                Post::create($item);
-            }
-            dd('created');
+            $data = request()->validate([
+                'title' => 'string',
+                'content' => 'string',
+                'image' => 'string',
+            ]);
+                Post::create($data);
+                return redirect()->route('post.index');
+        }
+
+        public function show(Post $post) {
+
+            return view('post.show', compact('post'));
         }
 
 
-        public function update()
+        public function edit(Post $post) {
+
+            return view('post.edit', compact('post'));
+        }
+
+        public function update(Post $post)
         {
-            $post = Post::find(6);
-            $post->update([
-                'title' => 'updated',
-                'content' => 'updated',
-                'image' => 'updated',
-                'likes' => 1000,
-                'is_published' => 0,
+             $data = request()->validate([
+                'title' => 'string',
+                'content' => 'string',
+                'image' => 'string',
             ]);
-            dd('update');
+                $post->update($data);
+                    return redirect()->route('post.show', $post->id);
         }
 
         public function delete()
         {
             $post = Post::withTrashed()->find(2);
             $post->restore();
-            dd('Данные восстановлены!');
+            dd('deleted!');
+        }
+
+
+        public function destroy(Post $post) 
+        {
+            $post->delete();
+            return redirect()->route('post.index');
         }
 
 
